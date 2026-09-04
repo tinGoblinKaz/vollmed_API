234ac29368495404d974277d1496514b5a8d3e45
Lucas A <kasalves333@gmail.com>
2026-09-04 11:20:14 -0300

    aula 04/09/2026

--- src/main/java/br/com/sistema/api/controller/MedicoController.java
+++ src/main/java/br/com/sistema/api/controller/MedicoController.java
@@ -1,5 +1,25 @@
 package br.com.sistema.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@RestController 
+@RequestMapping ("medico")
 public class MedicoController {
+    
+    @GetMapping("/") 
+    public String exibirHome() {
+        return "a";
+    };
+
+    @PostMapping("/cadastro")
+    public String cadastroMedico(String dados) {
+        
+        return dados;
+    }
+    
     
 }
